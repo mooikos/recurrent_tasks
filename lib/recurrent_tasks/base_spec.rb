1f3e6c5b21a955b01a7a8f8772fb7b7d60a5d1a8
Massimiliano De Vivo <massimiliano.devivo>
2025-01-14 10:19:38 +0100

    renamings

--- lib/recurrent_tasks/base_spec.rb
+++ lib/recurrent_tasks/base_spec.rb
@@ -14,18 +14,18 @@ task_files_paths.each do |task_file_path|
 
   puts "Declaring Recurrent Tasks: #{title}"
   describe title do
-    tasks = task_file_json.fetch('tasks', nil)
-    raise "The task file '#{task_file_path}' is missing a 'tasks' list property" unless tasks
+    recurrent_tasks = task_file_json.fetch('recurrent_tasks', nil)
+    raise "The task file '#{task_file_path}' is missing a 'recurrent_tasks' list property" unless recurrent_tasks
 
-    tasks.each_pair do |task_name, task_metadata|
-      documentation_url = task_metadata.fetch('documentation_url', "Missing 'documentation_url' property")
+    recurrent_tasks.each_pair do |recurrent_task_name, recurrent_task_metadata|
+      documentation_url = recurrent_task_metadata.fetch('documentation_url', "Missing 'documentation_url' property")
 
-      it "#{task_name} | #{documentation_url}" do
-        frequency_string = task_metadata.fetch('frequency', nil)
-        raise "The task '#{task_name}' inside '#{task_file_path}' is missing a 'frequency' property" unless frequency_string
+      it "#{recurrent_task_name} | #{documentation_url}" do
+        frequency_string = recurrent_task_metadata.fetch('frequency', nil)
+        raise "The task '#{recurrent_task_name}' inside '#{task_file_path}' is missing a 'frequency' property" unless frequency_string
 
         frequency = time_frequency_parser.parse_frequency frequency_string
-        last_checked = Time.parse task_metadata['last_checked']
+        last_checked = Time.parse recurrent_task_metadata['last_checked']
 
         expect(last_checked + frequency)
           .to be > Time.now, "The last time this task was done was '#{last_checked}'" \
